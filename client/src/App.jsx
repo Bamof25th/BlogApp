86f8cb2c2fc1065cee84cd91b229fe3edf934afe
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-01-29 23:16:59 +0530

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import About from './pages/About';
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline justify-center flex mt-16">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/signup' element={<Signup />}/>
+        <Route path='/signin' element={<Signin />}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/projects' element={<Projects />}/>
+        <Route path='/about' element={<About />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+    <h1 className="">
+    Projects
+    </h1>
+    </div>
+  )
+}
+
+export default Projects
