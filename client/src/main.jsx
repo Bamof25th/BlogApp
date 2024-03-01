439979b905e961318541b85063ff808a19fc75be
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-03-01 22:20:45 +0530

    react-persistant

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./app/store.js";
+import { store, persistior } from "./app/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistior}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
