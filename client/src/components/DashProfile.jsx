c50f1d05ecd695c137035bc900a7b43e0bb80567
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-03-03 17:20:24 +0530

    made sidebar  of dashboard

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,8 @@
+
+const DashProfile = () => {
+  return (
+    <div className=" ">DashProfile</div>
+  )
+}
+
+export default DashProfile
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 
 const Dashboard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabUrlForm = urlParams.get("tab");
+    if (tabUrlForm) {
+      setTab(tabUrlForm);
+    }
+  }, [location.search]);
 
-export default Dashboard
+  return (
+    <>
+      <div className="min-h-screen flex flex-col md:flex-row">
+        <div className="md:w-56">
+          {/* sidebar */}
+          <DashSidebar />
+        </div>
+        {/* Profile */}
+        {tab === "profile" && <DashProfile />}
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
