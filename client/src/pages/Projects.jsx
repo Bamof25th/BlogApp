bd97bc57e63aa64ae16ddd74db5fd710e1f38aca
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-03-13 01:21:21 +0530

    coplete about and projects pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "./../components/CallToAction";
 
 const Projects = () => {
   return (
-    <div>
-    <h1 className="">
-    Projects
-    </h1>
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold ">Projects</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning HTML, CSS, and
+        JavaScript!
+      </p>
+      <CallToAction />
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
